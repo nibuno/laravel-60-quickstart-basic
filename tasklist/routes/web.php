a7bdd01e845ded0599d9a4d1310af335689a6856
Yumihiki <nibu10.majik@gmail.com>
2019-09-16 17:01:35 +0900

    use構文追記とタスク追記文

--- tasklist/routes/web.php
+++ tasklist/routes/web.php
@@ -1,5 +1,6 @@
 <?php
-
+use App\Task;
+use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -11,6 +12,44 @@
 |
 */
 
+/**
+ * タスクダッシュボード表示
+ */
+
 Route::get('/', function () {
-    return view('welcome');
+    $tasks = Task::orderBy('created_at', 'asc')->get();
+
+    return view('tasks', [
+        'tasks' => $tasks
+    ]);
 });
+
+/**
+ * 新タスク追加
+ */
+
+ Route::post('/task', function (Request $request) {
+     $validator = Validator::make($request->all(), [
+         'name' => 'required|max:255',
+     ]);
+
+     if ($validator->fails()) {
+         return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+     }
+
+     $task = new Task();
+     $task->name = $request->name;
+     $task->save();
+
+     return redirect('/');
+ });
+
+ /**
+  * タスク削除
+  */
+
+  Route::delete('/task/{task}', function (Task $task) {
+      //
+  });
